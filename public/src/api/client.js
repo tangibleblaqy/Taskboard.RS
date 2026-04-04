6178944e56a1c71a56f06aee0893d716b362d818
Yixiao <zhanyixiao06@gmail.com>
2026-04-04 17:12:56 +0200

    finalizar finalizar frontend con conexion a backend y codigo original reescrito

--- public/src/api/client.js
+++ public/src/api/client.js
@@ -1,7 +1,11 @@
-const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:3000/api/v1/' : '/api/v1/';
+const API_URL =
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1"
+    ? "http://localhost:3000/api/v1/"
+    : "/api/v1/";
 
 const toast = document.getElementById("toast");
-function toast(message, type = "info") {
+function showToast(message, type = "info") {
   toast.textContent = message;
 
   toast.className = "toast";
@@ -15,34 +19,39 @@ function toast(message, type = "info") {
 }
 
 async function fetchApi(endpoint, options = {}) {
-    const url = `${API_URL}${endpoint}`;
-    const config = {
-        method: options.method || 'GET',
-        ...options,
-        headers: {
-            'Content-Type': 'application/json',
-            ...options.headers,
-        },
-        body: options.body ? JSON.stringify(options.body) : undefined,
-    };
+  const url = `${API_URL}${endpoint}`;
+  const config = {
+    method: options.method || "GET",
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      ...options.headers,
+    },
+    body: options.body ? JSON.stringify(options.body) : undefined,
+  };
 
-    try {
-        const response = await fetch(url, config);
-        if (!response.ok) {
-            const textoError = await response.json().catch(() => null);
-            throw new Error(textoError?.message || `Algo salió mal: ${response.status}`);
-            toast(textoError?.message || `Algo salió mal: ${response.status}`, "error");
-        }
-        if (response.status === 204) return null;
-        toast("Operación exitosa", "success");
-        return await response.json().catch(() => null);
-    } catch (error) {
-        console.error(error.message);
-        toast(error.message, "error");
-        throw error;
+  try {
+    const response = await fetch(url, config);
+    if (!response.ok) {
+      const textoError = await response.json().catch(() => null);
+      showToast(
+        textoError?.error || `Algo salió mal: ${response.status}`,
+        "error",
+      );
+      throw new Error(
+        textoError?.error || `Algo salió mal: ${response.status}`,
+      );
     }
+    if (response.status === 204) return null;
+    showToast("Operación exitosa", "success");
+    return await response.json().catch(() => null);
+  } catch (error) {
+    console.error(error.message);
+    showToast(error.message, "error");
+    throw error;
+  }
 }
 
 export async function apiRequest(endpoint, options = {}) {
-    return await fetchApi(endpoint, options);
+  return await fetchApi(endpoint, options);
 }
--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,4 +1,4 @@
-﻿import { apiRequest } from "../api/client";
+﻿import { apiRequest } from "../api/client.js";
 
 const seccionBuscador = document.getElementById("seccionBuscador");
 const buscador = document.getElementById("buscador");
@@ -25,288 +25,365 @@ const tareasSkilling = document.getElementById("tareasSkilling");
 const tareasBossing = document.getElementById("tareasBossing");
 const otrasTareas = document.getElementById("otrasTareas");
 
+const textoFelicitacion = document.getElementById("textoFelicitacion");
+const baile = document.getElementById("baile");
+
+let tareasGuardadas = [];
 //cargar tareas
 const cargarTareas = async () => {
-  const tareasGuardadas = await apiRequest("/", {})
+  const tareas = await apiRequest("tareas", {});
+  tareasGuardadas.push(...tareas);
   console.log(tareasGuardadas);
-}
+  tareasGuardadas.forEach((tarea) => crearTarea(tarea));
+  mostrarInformacion();
+  actualizarEstadisticas();
+};
 
-//borrar valores del formulario
-const borrarFormulario = () => {
-  creador.value = "";
-}
+cargarTareas();
+
+const borrarTarea = async (id) => {
+  return await apiRequest(`tareas/${id}`, {
+    method: "DELETE",
+  });
+};
+
+const editarTarea = async (tarea) => {
+  return await apiRequest(`tareas/${tarea.id}`, {
+    method: "PATCH",
+    body: tarea,
+  });
+};
+
+const completarTarea = async (id) => {
+  return await apiRequest(`tareas/completados/${id}`, {
+    method: "PATCH",
+  });
+};
+
+//crear tarea en backend
+const crearTareaBackend = async (nombre, categoria) => {
+  return await apiRequest("tareas", {
+    method: "POST",
+    body: { nombre, categoria },
+  });
+};
 
 //funcion para crear tareas
 const crearTarea = (tarea) => {
   const div = document.createElement("div");
-  div.className = "tarea p-1 flex flex-row items-center max-w-full border";
+  div.id = tarea.id;
+  div.className = "tarea";
   div.classList.add(tarea.categoria);
 
   const p = document.createElement("p");
-  p.textContent = tarea.valor;
-  p.dataset.valor = tarea.valor;
-  p.className = "flex-1 p-1.5 whitespace-nowrap overflow-x-auto";
-  if (tarea.completado){
+  p.textContent = tarea.nombre;
+  p.dataset.valor = tarea.nombre;
+  if (tarea.completada === true) {
     p.classList.add("line-through");
+    div.classList.add("completado");
   }
 
   const img = new Image(20, 20);
-  if (tarea.categoria === "skilling"){
-    img.src = "/recursos/Stats_icon.png"
-  } else if (tarea.categoria === "bossing"){
-    img.src = "/recursos/Multicombat.png"
-  } else if(tarea.categoria === "otro"){
-    img.src = "/recursos/Dungeon_icon.png"
-  } else {
-    img.src = "/recursos/Distraction_map_icon.png"
+  if (tarea.completada === true) {
+    img.src = "assets/Distraction_map_icon.png";
+  } else if (tarea.categoria === "skilling") {
+    img.src = "assets/Stats_icon.png";
+  } else if (tarea.categoria === "bossing") {
+    img.src = "assets/Multicombat.png";
+  } else if (tarea.categoria === "otros") {
+    img.src = "assets/Dungeon_icon.png";
   }
-
   const borrar = document.createElement("button");
   borrar.textContent = "✕";
-  borrar.className = "borrar border botonPeq hoverRojo";
-  borrar.style.margin = "0.125rem";  
+  borrar.className = "borrar";
 
   //eventListener para boton de borrar
-  borrar.addEventListener("click", () => {
-    const valorP = div.querySelector("p").dataset.valor;
-    tareasGuardadas = tareasGuardadas.filter(tarea => tarea.valor !== valorP);
-    actualizarLocal();
+  borrar.addEventListener("click", async () => {
+    const tareaId = div.id;
+    await borrarTarea(tareaId);
+    tareasGuardadas = tareasGuardadas.filter((tarea) => tarea.id !== tareaId);
     div.remove();
     mostrarInformacion();
     actualizarEstadisticas();
-  })
-  
-  //si no esta completo
-  if(tarea.categoria !== "completado"){
+  });
 
+  //si no esta completo
+  if (tarea.completada !== true) {
     const editar = document.createElement("button");
     editar.textContent = "✎";
-    editar.className = "editar border botonPeq hoverNeutro";
-    editar.style.margin = "0.125rem";
+    editar.className = "editar";
 
     //eventListener para boton de editar
     editar.addEventListener("click", () => {
-        const valorP = prompt("Editar tarea:", p.textContent);
-        if (valorP !== null) {
-        if(valorP === ""){
+      const valorP = prompt("Editar tarea:", p.textContent);
+      if (valorP !== null) {
+        if (valorP === "") {
           alert("no puede estar vacío");
           return;
         }
-        const editar = tareasGuardadas.find(t => t.valor === p.dataset.valor);
-        if (editar) editar.valor = valorP;
-        p.textContent = valorP;
-        p.dataset.valor = valorP;
-        actualizarLocal();
+        const tareaId = event.target.closest("div").id;
+        const tarea = tareasGuardadas.find((t) => t.id === tareaId);
+        tarea.nombre = valorP;
+        let tareaEditada = {};
+        const edición = async () => {
+          tareaEditada = await editarTarea(tarea);
+          console.log(tareaEditada);
+          Object.assign(tarea, tareaEditada);
+          p.textContent = tareaEditada.nombre;
+          p.dataset.valor = tareaEditada.nombre;
+        };
+        edición();
         actualizarEstadisticas();
       }
       return;
-    })
+    });
 
     const completar = document.createElement("button");
     completar.textContent = "✓";
-    completar.className = "completar border botonPeq hoverVerde";
-    completar.style.margin = "0.125rem";
+    completar.className = "completar";
 
- 
     //eventListener para boton de completar
-    completar.addEventListener("click", () => {
+    completar.addEventListener("click", async () => {
+      const tareaId = event.target.closest("div").id;
+      const tarea = tareasGuardadas.find((t) => t.id === tareaId);
+      if (tarea) {
+        tarea.completada = true;
+        div.classList.add("completado");
+      }
+      await completarTarea(tareaId);
       p.classList.add("line-through");
-      p.classList.add("completado");
-      div.classList.remove("skilling", "bossing", "otro");
-      div.classList.add("completado");
-      const tareaTemporal = tareasGuardadas.find(tarea => tarea.valor === p.textContent);
-      tareaTemporal.categoria = "completado";
       const imagen = div.querySelector("img");
-      imagen.src = "/recursos/Distraction_map_icon.png?t=" + Date.now();
+      imagen.src = "assets/Distraction_map_icon.png";
       completar.remove();
       editar.remove();
-      actualizarLocal();
       actualizarEstadisticas();
-      })
+    });
 
-      div.append(img, p, borrar, editar, completar);
-  }else{
+    div.append(img, p, borrar, editar, completar);
+  } else {
     div.append(img, p, borrar);
   }
-    document.getElementById("tareas").appendChild(div);
-    if (filtroActual && !div.classList.contains(filtroActual)) {
-      div.classList.add("hidden");
-    }
-}
+  document.getElementById("tareas").appendChild(div);
+  if (filtroActual && !div.classList.contains(filtroActual)) {
+    div.style.display = "none";
+  }
+};
 
 //Marcar todo como completado
-const todoCompletado = () => {  
-  const respuesta = confirm("¿Estás seguro de que quieres marcar todas las tareas como completadas?");
+const todoCompletado = async () => {
+  const respuesta = confirm(
+    "¿Estás seguro de que quieres marcar todas las tareas como completadas?",
+  );
   if (respuesta) {
-      const divTareas = document.getElementById("tareas");
-      const todoTarea = divTareas.querySelectorAll("div p");
-      todoTarea.forEach((p) => {
-      p.classList.add("completado", "line-through")
+    await apiRequest("tareas/completados", {
+      method: "PATCH",
+    });
+
+    const divTareas = document.getElementById("tareas");
+    const todoTarea = divTareas.querySelectorAll("div p");
+    todoTarea.forEach((p) => {
+      p.classList.add("completado", "line-through");
       const parentDiv = p.closest("div");
       parentDiv.classList.remove("skilling", "bossing", "otro");
       parentDiv.classList.add("completado");
-      const tareaTemporal = tareasGuardadas.find(tarea => tarea.valor === p.textContent);
-      tareaTemporal.categoria = "completado";
-      actualizarLocal();
+      const tareaTemporal = tareasGuardadas.find(
+        (tarea) => tarea.id === parentDiv.id,
+      );
+      if (tareaTemporal) {
+        tareaTemporal.completada = true;
+      }
     });
     const imagen = divTareas.querySelectorAll("img");
     imagen.forEach((img) => {
-      img.src = "/recursos/Distraction_map_icon.png?t=" + Date.now()
-      divTareas.querySelectorAll("div button:nth-child(4), div button:nth-child(5)").forEach(boton => boton.remove());;
-    })
-  }
-}
+      img.src = "assets/Distraction_map_icon.png?t=" + Date.now();
+    });
+    divTareas
+      .querySelectorAll("div button.editar, div button.completar")
+      .forEach((boton) => boton.remove());
 
-completarTodo.addEventListener("click", () =>{
+    actualizarEstadisticas();
+    filtrarTareas(filtroActual);
+  }
+};
+
+completarTodo.addEventListener("click", () => {
   todoCompletado();
   actualizarEstadisticas();
-})
+});
 
 //borrar todo completado:
-const borrarCompletado = () => {
-  const respuesta = confirm("¿Estás seguro de que quieres borrar todas las tareas completadas?");  
+const borrarCompletado = async () => {
+  const respuesta = confirm(
+    "¿Estás seguro de que quieres borrar todas las tareas completadas?",
+  );
   if (respuesta) {
-    const divTareas = document.getElementById("tareas");
-      divTareas.querySelectorAll(".completado").forEach(tarea =>{
-      const valorP = tarea.dataset.valor;
-      tareasGuardadas = tareasGuardadas.filter(t => t.valor !== valorP);
-      tarea.parentElement.remove();
-      actualizarLocal();
-    })
+    await apiRequest("tareas/completados", {
+      method: "DELETE",
+    });
+
+    // Remove completed tasks from DOM and array
+    tareasGuardadas.forEach((tarea) => {
+      if (tarea.completada) {
+        const div = document.getElementById(tarea.id);
+        if (div) div.remove();
+      }
+    });
+    tareasGuardadas = tareasGuardadas.filter((t) => !t.completada);
     mostrarInformacion();
     actualizarEstadisticas();
+    filtrarTareas(filtroActual);
   }
-}
+};
 
-borrarTodo.addEventListener("click", () =>{
+borrarTodo.addEventListener("click", () => {
   borrarCompletado();
-})
+});
+
+//Crear tarea
+const formularioTarea = document.getElementById("formularioTarea");
+formularioTarea.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const tareaTemporal = { categoria: tipo.value, nombre: creador.value.trim() };
+
+  const nuevaTarea = await crearTareaBackend(
+    tareaTemporal.nombre,
+    tareaTemporal.categoria,
+  );
+  tareasGuardadas.push(nuevaTarea);
+  crearTarea(nuevaTarea);
+  borrarFormulario();
+  mostrarInformacion();
+  actualizarEstadisticas();
+});
+
+//borrar valores del formulario
+const borrarFormulario = () => {
+  creador.value = "";
+};
 
 //funcion para buscar tarea
 const buscarTarea = () => {
-    const valorBuscador = buscador.value;
-    document.querySelectorAll(".tarea").forEach(div => {
+  const valorBuscador = buscador.value;
+  document.querySelectorAll(".tarea").forEach((div) => {
     const p = div.querySelector("p");
+    console.log(p);
     const tarea = p.textContent.toLowerCase();
-    
-    //presentacion de la tarea
+
     if (tarea.includes(valorBuscador)) {
-      div.classList.remove("hidden");
+      div.style.display = "";
     } else {
-      div.classList.add("hidden");;
+      div.style.display = "none";
     }
-  })
-}
+  });
+};
+
+//Buscador
+buscador.addEventListener("input", buscarTarea);
 
 //filtrar por categoria
 let filtroActual = null;
 const botones = [completados, skilling, bossing, otro];
 
 const filtrarTareas = (categoria) => {
-  const tarea = tareas.querySelectorAll("div");
-  tarea.forEach(valor => {
-    if (categoria === null || valor.classList.contains(categoria)) {
-      valor.classList.remove("hidden");
+  const tareaDivs = tareas.querySelectorAll("div.tarea");
+  tareaDivs.forEach((div) => {
+    if (categoria === null) {
+      div.style.display = "";
+    } else if (categoria === "completado") {
+      div.style.display = div.classList.contains("completado") ? "" : "none";
     } else {
-      valor.classList.add("hidden");
+      div.style.display =
+        div.classList.contains(categoria) &&
+        !div.classList.contains("completado")
+          ? ""
+          : "none";
     }
   });
-}
+};
 
 const activarBoton = (boton, categoria) => {
   filtroActual = filtroActual === categoria ? null : categoria;
-  botones.forEach(b => b.classList.remove("elegido"));
+  botones.forEach((b) => b.classList.remove("elegido"));
   if (filtroActual) {
     boton.classList.add("elegido");
   }
   filtrarTareas(filtroActual);
-}
+};
 
-completados.addEventListener("click", () =>{
+completados.addEventListener("click", () => {
   activarBoton(completados, "completado");
-})
+});
 
-skilling.addEventListener("click", () =>{
+skilling.addEventListener("click", () => {
   activarBoton(skilling, "skilling");
-})
+});
 
-bossing.addEventListener("click", () =>{
+bossing.addEventListener("click", () => {
   activarBoton(bossing, "bossing");
-})
+});
 
-otro.addEventListener("click", () =>{
-  activarBoton(otro, "otro");
-})
+otro.addEventListener("click", () => {
+  activarBoton(otro, "otros");
+});
 
-//Buscador
-buscador.addEventListener("input", (buscarTarea));
-
-//Crear tarea
-formularioTarea.addEventListener("submit", (e) =>{
-    e.preventDefault();
-    
-    const tareaTemporal = {categoria: tipo.value, valor: creador.value};
-
-    //comprobar que no sea repetido o vacio
-    if (tareaTemporal.valor === "") {
-    borrarFormulario();
-    return alert("La tarea no puede estar vacío");
-    } else if(tareasGuardadas.some((value) => value.valor.toLowerCase() === tareaTemporal.valor.toLowerCase())){
-    borrarFormulario();
-    return alert("No puede haber tareas repetidas"); 
-    }
-
-
-    tareasGuardadas.push(tareaTemporal);
-    crearTarea(tareaTemporal);
-    actualizarLocal();
-    borrarFormulario();
-    mostrarInformacion();
-    actualizarEstadisticas();
-})
-
-//Funcion para mostrar informacion, si no hay tarea muestra la imagen
+//Funcion para mostrar informacion
 const mostrarInformacion = () => {
-  if (!tareasGuardadas.length){
-    seccionBuscador.classList.add("hidden");
-    estadisticas.classList.add("hidden");
-    filtros.classList.add("hidden");
-    botonTodo.classList.add("hidden");
-    chatHead.classList.remove("hidden");
-  }else{
-    seccionBuscador.classList.remove("hidden");
-    estadisticas.classList.remove("hidden");
-    filtros.classList.remove("hidden");
-    botonTodo.classList.remove("hidden");
-    chatHead.classList.add("hidden");
+  if (!tareasGuardadas.length) {
+    seccionBuscador.style.display = "none";
+    estadisticas.style.display = "none";
+    filtros.style.display = "none";
+    botonTodo.style.display = "none";
+    chatHead.style.display = "";
+  } else {
+    seccionBuscador.style.display = "";
+    estadisticas.style.display = "";
+    filtros.style.display = "";
+    botonTodo.style.display = "";
+    chatHead.style.display = "none";
   }
-}
+};
+
+//estadisticas
+const actualizarEstadisticas = () => {
+  tareasTotales.textContent = tareasGuardadas.length;
+  tareasCompletas.textContent = tareasGuardadas.reduce(
+    (contador, tarea) => (tarea.completada === true ? contador + 1 : contador),
+    0,
+  );
+  tareasSkilling.textContent = tareasGuardadas.reduce(
+    (contador, tarea) =>
+      tarea.categoria === "skilling" ? contador + 1 : contador,
+    0,
+  );
+  tareasBossing.textContent = tareasGuardadas.reduce(
+    (contador, tarea) =>
+      tarea.categoria === "bossing" ? contador + 1 : contador,
+    0,
+  );
+  otrasTareas.textContent = tareasGuardadas.reduce(
+    (contador, tarea) =>
+      tarea.categoria === "otros" ? contador + 1 : contador,
+    0,
+  );
+  felicitar();
+};
+
+//felicitar
+const felicitar = () => {
+  const total = parseInt(tareasTotales.textContent);
+  const completas = parseInt(tareasCompletas.textContent);
+  if (total > 0 && total === completas) {
+    textoFelicitacion.style.display = "";
+    baile.style.display = "block";
+  } else {
+    textoFelicitacion.style.display = "none";
+    baile.style.display = "none";
+  }
+};
 
 //Modo oscuro
 const tema = document.getElementById("tema");
 tema.addEventListener("click", () => {
   document.documentElement.classList.toggle("dark");
-})
-
-//estadisticas
-const actualizarEstadisticas = () => {
-  tareasTotales.textContent = tareasGuardadas.length;
-  tareasCompletas.textContent = tareasGuardadas.reduce((contador, tarea) => tarea.categoria === "completado" ? contador + 1 : contador, 0);
-  tareasSkilling.textContent = tareasGuardadas.reduce((contador, tarea) => tarea.categoria === "skilling" ? contador + 1 : contador, 0);
-  tareasBossing.textContent = tareasGuardadas.reduce((contador, tarea) => tarea.categoria === "bossing" ? contador + 1 : contador, 0);
-  otrasTareas.textContent = tareasGuardadas.reduce((contador, tarea) => tarea.categoria === "otro" ? contador + 1 : contador, 0);
-  felicitar();
-}
-
-//felicitar
-const felicitar = () => {
-  if (tareasTotales.textContent === tareasCompletas.textContent){
-    const textoFelicitacion = document.getElementById("textoFelicitacion");
-    const baile = document.getElementById("baile");
-    textoFelicitacion.classList.remove("hidden");
-    baile.classList.remove("hidden");
-  } else{
-    textoFelicitacion.classList.add("hidden");
-    baile.classList.add("hidden");
-  }
-}
+});
